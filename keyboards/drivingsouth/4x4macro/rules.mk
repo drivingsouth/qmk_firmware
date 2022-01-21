b4b78d9be18fd0dcb92e31715054235e61d12ce1
REVOLVER\tm <drivingsouth1994@gmail.com>
2022-01-22 05:42:23 +0900

    checked basic functions

---
+++ keyboards/drivingsouth/4x4macro/rules.mk
@@ -0,0 +1,11 @@
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+
+DEFAULT_FOLDER = drivingsouth/4x4macro/f411
